6436e8872097b54df6fd6bd81a12541c5ba1a325
mGrochowski <mateusz.grochowski1992@gmail.com>
2022-07-27 19:13:25 +0200

    Add Svg Injector

---
+++ backup/src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Tutaj będzie tabelka
+
+    <Scheduler></Scheduler>
+  </div>
+</template>
+
+<script setup>
+import Scheduler from './Scheduler/Index.vue'
+</script>
---
+++ backup/src/components/Scheduler/Index.vue
@@ -0,0 +1,10 @@
+﻿<template>
+  <div>TABLE</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
