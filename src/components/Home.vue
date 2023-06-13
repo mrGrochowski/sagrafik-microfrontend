6e547116c41aeab80fe4b083dd11fffaec6f16dc
mGrochowski <mateusz.grochowski1992@gmail.com>
2023-06-13 20:02:30 +0200

    Set black theme

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,11 @@
   <div class="flex flex-col items-center">
     <!-- Tutaj będzie tabelka
     {{ headers }} -->
-    <div v-for="card in cards" :key="card['Sygnatura czasowa']" class="bg-white shadow-xl shadow-gray-100 w-full max-w-4xl px-5 py-4 rounded-xl mb-10 break-all">
+    <div class="card">
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl text-emerald-300">Witaj w SA Grafik <br/>Online</h1>
+    <h4>Wszystkie Meetingi w Polsce na CITO 🔥</h4>
+    </div>
+    <div v-for="card in cards" :key="card['Sygnatura czasowa']" class="card">
       <div v-for="(elem, label) in card" :key="elem">
         <span> {{ label }}: </span>
         <strong v-if="elem.match('http')">
@@ -18,6 +22,11 @@
   </div>
 </template>
 
+<style lang="scss" scoped>
+  .card {
+    @apply bg-gray-100 shadow-xl shadow-gray-800 w-full max-w-4xl px-5 py-4 rounded-xl mb-10 break-all
+  }
+</style>
 <script setup>
 import { ref, onMounted } from 'vue'
 import { sync, Storage, getRows, getHeaders, getPreparedCards } from '../composables/fetchMeetings.js'
