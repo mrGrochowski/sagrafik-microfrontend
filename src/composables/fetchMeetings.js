0346e6905141a6fe146d7f8756b1710edcd9e67c
mGrochowski <mateusz.grochowski1992@gmail.com>
2023-06-13 11:31:28 +0200

    add few tweaks in code

--- src/composables/fetchMeetings.js
+++ src/composables/fetchMeetings.js
@@ -1,46 +1,48 @@
-const { VITE_DATA_SOURCE, VITE_APP_KEY } = import.meta.env
-import { decrypt } from './decryptContent.js';
-import cloneDeep from 'lodash/cloneDeep.js';
+const { VITE_DATA_SOURCE, VITE_APP_KEY, VITE_DATA_SHEET } = import.meta.env
+import { decrypt } from './decryptContent.js'
+import cloneDeep from 'lodash/cloneDeep.js'
 
 export let Storage = {}
 
 export const sync = async () => {
-  const res = await fetch(`${VITE_DATA_SOURCE}/values/protected!A:ZZ?key=${VITE_APP_KEY}`)
+  const res = await fetch(`${VITE_DATA_SOURCE}/values/${VITE_DATA_SHEET}!A:ZZ?key=${VITE_APP_KEY}`)
   const fetchJson = await res.json()
   Storage = fetchJson
   return fetchJson
 }
+const decryptResponse = () =>
+  Storage.values
+    .filter((element, index) => {
+      //ommit heading section of table
+      return index > 0
+    })
+    .map((row) => row.map((col) => decrypt(col)))
 
-export const prepareRows = () => {
-  
-  const decryptedResponse = Storage.values.filter((element, index) => {
-    //ommit heading section of table
-    return index > 0
-  }).map(row => row.map(col => decrypt(col)))
-  
-  const cloneDecryptedResponse = cloneDeep(decryptedResponse);
-  
-  const regex = /\d{4}\-\d{2}\-\d{2}T/
-  const responseWithTimestampsAsHHMM = cloneDecryptedResponse 
-    .map(row =>
+const changeISODateToHoursInResponse = (decryptedResponse) => {
+    const regex = /\d{4}\-\d{2}\-\d{2}T/
+    const responseWithTimestampsAsHHMM = decryptedResponse.map((row) =>
       cloneDeep(row).map((col) => {
         //get just hour format
-        const condition = regex.test(col);
-        console.log(`${col} ${condition}`);
-
+        const condition = regex.test(col)
         if (condition) {
-            //regex.test(col);
-            const res =  col.match(/\d\d:\d\d/)["0"];
-            return res
+          const res = col.match(/\d\d:\d\d/)['0']
+          return res
         }
         return col
       })
-  )
-  
-  return responseWithTimestampsAsHHMM;
+    )
+
+    return responseWithTimestampsAsHHMM
+  }
+
+export const prepareRows = () => {
+  Storage.decryptedResponse = decryptResponse()
+  Storage.responseWithFormatedDates = changeISODateToHoursInResponse(Storage.decryptedResponse) 
+
+  return Storage.responseWithFormatedDates
 }
 
-export const getRows = prepareRows; 
+export const getRows = prepareRows
 export const getHeaders = () => Storage.values && Storage.values[0]
 
 export const getPreparedCards = () => {
