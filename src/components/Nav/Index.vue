ee8731452832183a9abc042ad7604d5bc4e473e8
mGrochowski <mateusz.grochowski1992@gmail.com>
2023-06-15 13:39:33 +0200

    MVP ready

--- src/components/Nav/Index.vue
+++ src/components/Nav/Index.vue
@@ -1,9 +1,8 @@
 <template>
   <!-- component -->
 
-  <!-- <section id="bottom-navigation" class="md:hidden block fixed inset-x-0 bottom-0 z-10 bg-white shadow"> // if shown only tablet/mobile-->
-  <section id="bottom-navigation" class="block fixed inset-x-0 bottom-0 z-10 bg-emerald-300 shadow">
-    <div id="tabs" class="flex justify-between">
+  <section id="bottom-navigation" class="block fixed inset-x-0 bottom-0 z-10 bg-emerald-300 shadow md:w-1/12 md:max-w-min md:h-full">
+    <div id="tabs" class="flex justify-between md:flex-col">
       <router-link to="/" class="w-full focus:text-teal-500 focus:bg-zinc-900 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
         <!-- <IconHomeSvg class="inline-block mb-1" /> -->
         <span class="inline-block h-7 w-7"><meetingIcon /></span>
