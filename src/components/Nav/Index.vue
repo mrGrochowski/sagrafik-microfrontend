0d1de0ba10c09d393b36f7d116f2aab1a0f44359
mGrochowski <mateusz.grochowski1992@gmail.com>
2023-06-19 16:58:52 +0200

    hotfix for favicon ect

--- src/components/Nav/Index.vue
+++ src/components/Nav/Index.vue
@@ -3,6 +3,7 @@
 
   <section id="bottom-navigation" class="block fixed inset-x-0 bottom-0 z-10 bg-emerald-300 shadow md:w-1/12 md:max-w-min md:h-full">
     <div id="tabs" class="flex justify-between md:flex-col">
+      <div id="logo" class="w-full text-zinc-900 focus:text-teal-500 focus:bg-zinc-900 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1"><saLogo></saLogo></div>
       <router-link to="/" class="w-full focus:text-teal-500 focus:bg-zinc-900 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
         <!-- <IconHomeSvg class="inline-block mb-1" /> -->
         <span class="inline-block h-7 w-7"><meetingIcon /></span>
@@ -19,6 +20,6 @@
 <script setup>
 import IconHomeSvg from './IconHomeSvg.vue'
 import IconCategorySvg from './IconCategorySvg.vue'
-
+import saLogo from '../../../public/logo.svg'
 import meetingIcon from '../../../public/meeting2.svg'
 </script>
