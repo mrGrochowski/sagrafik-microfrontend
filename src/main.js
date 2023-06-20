66b989b8187543c413fed1fda540c7ab918bcc85
mGrochowski <mateusz.grochowski1992@gmail.com>
2023-06-20 13:40:43 +0200

    add eruda for dev purpose

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './index.css'
+import eruda from 'eruda';
 
 const { VITE_DEVMODE } = import.meta.env
 
 if (VITE_DEVMODE) {
   const link = document.querySelector("link[rel~='icon']")
-  link.href = '/favicon_DEV.ico'
+  link.href = '/favicon_DEV.ico';
+
+  eruda.init();
 }
 createApp(App).use(router).use(store).mount('#app')
