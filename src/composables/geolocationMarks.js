8403ac4dfc6f0f9bc664f49b78a1b102a2d27503
mGrochowski <mateusz.grochowski1992@gmail.com>
2023-06-27 14:43:39 +0200

    fix for bs

--- src/composables/geolocationMarks.js
+++ src/composables/geolocationMarks.js
@@ -3,6 +3,6 @@ export const getLatLonFromCityName = async (cityName) => {
     const request = await fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${cityName}&format=json&apiKey=1b48259b810e48ddb151889f9ea58db0`);
     
     const parse = await request.json();
-    const {lat,lon} = parse.results[0]
+    const { lat, lon } = parse.results[0] || [false, false] 
     return {lat,lon}
 }
