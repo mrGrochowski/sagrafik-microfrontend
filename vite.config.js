215298c1e2a91daede548d3bbbee8a12fe102276
mrGrochowski <mateusz.grochowski1992@gmail.com>
2023-12-02 22:02:18 +0100

    ADD basics

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,44 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
-
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     svgLoader({
       svgo: false,
+    }),
+    VitePWA({
+      registerType: 'prompt',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'sagrafik.online',
+        short_name: 'SAgrafik',
+        description: 'Meetingi w całej Polsce na CITO',
+        theme_color: '#27272a',
+        icons: [  
+          {  
+            src: 'logo.png',  
+            sizes: '192x192',  
+            type: 'image/png',  
+          },  
+          {  
+            src: 'logo.png',  
+            sizes: '512x512',  
+            type: 'image/png',  
+          },  
+          {  
+            src: 'logo.png',  
+            sizes: '512x512',  
+            type: 'image/png',  
+            purpose: 'any maskable',  
+          },  
+        ],
+      }
     }),
   ],
   server: {
