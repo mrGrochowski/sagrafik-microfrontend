d9332b633a04fef0bfce5d77de6b8a49fdea65c7
mrGrochowski <mateusz.grochowski1992@gmail.com>
2024-02-26 00:26:06 +0100

    add fix for pwa

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       svgo: false,
     }),
     VitePWA({
-      registerType: 'prompt',
+      registerType: 'autoUpdate',
       devOptions: {
         enabled: true
       },
