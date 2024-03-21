6b34abf188f9f1f8480fbff43b6001eec35f1d6e
mrGrochowski <mateusz.grochowski1992@gmail.com>
2024-03-21 22:01:40 +0100

    changes for dialog

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -4,7 +4,7 @@
     tabindex="-1"
     class="fixed isolate top-0 left-0 bottom-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-full flex justify-center items-center"
   >
-    <div class="fixed z-0 top-0 left-0 bottom-0 right-0 block backdrop-blur bg-white supports-backdrop-blur:bg-white/95 dark:bg-slate-900/75 " @click="$emit('close')"></div>
+    <div class="fixed z-0 top-0 left-0 bottom-0 right-0 block backdrop-blur bg-slate-900/75 " @click="$emit('close')"></div>
     <div class="mt-3 p-3 pr-7 bg-slate-200 rounded-xl shadow-xl relative w-4/5 h-auto break-words shadow-[0_0px_60px_-15px_#99f6e4]">
     <span class="absolute right-3 bg-slate-200" @click="$emit('close')">✖</span>
       <slot></slot>
