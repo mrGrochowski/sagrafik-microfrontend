05394a8de63b910e168ccc91f6086cfd156dd037
mrGrochowski <mateusz.grochowski1992@gmail.com>
2024-03-21 22:08:59 +0100

    fix for password screen background

--- src/components/passwordDialog.vue
+++ src/components/passwordDialog.vue
@@ -5,10 +5,10 @@
     class="fixed top-0 left-0 right-0 bottom-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-full flex justify-center items-center"
   >
     <div
-      class="w-full h-full absolute top-0 left-0 block backdrop-blur bg-white supports-backdrop-blur:bg-white/95 dark:bg-slate-900/75"
+      class="w-full h-full absolute top-0 left-0 block backdrop-blur bg-slate-900/75"
     ></div>
     <div class="relative w-full max-w-md max-h-full">
-      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+      <div class="relative bg-white rounded-lg shadow bg-gray-700">
         <!-- <span
           class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
           data-modal-hide="popup-modal"
