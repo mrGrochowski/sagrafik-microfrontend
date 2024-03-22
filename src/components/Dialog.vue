a396f29ec1befdf4c21e51a7facf1d629e81e5bf
mrGrochowski <mateusz.grochowski1992@gmail.com>
2024-03-22 19:41:32 +0100

    fix for dialog

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -2,7 +2,7 @@
   <div
     id="dialog"
     tabindex="-1"
-    class="fixed isolate top-0 left-0 bottom-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-full flex justify-center items-center"
+    class="fixed isolate top-0 left-0 bottom-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-full flex justify-center items-start"
   >
     <div class="fixed z-0 top-0 left-0 bottom-0 right-0 block backdrop-blur bg-slate-900/75 " @click="$emit('close')"></div>
     <div class="mt-3 p-3 pr-7 bg-slate-200 rounded-xl shadow-xl relative w-4/5 h-auto break-words shadow-[0_0px_60px_-15px_#99f6e4]">
